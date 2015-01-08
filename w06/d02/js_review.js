2e0d1b5d7972548470d4d425515fda986db65972
Bernz <BernzAir@BernzAir.local>
2015-01-08 13:01:17 -0500

    lunch update

--- w06/d02/js_review.js
+++ w06/d02/js_review.js
@@ -1,38 +1,38 @@
 // EIEInope
-//
-var arg = process.argv[2].toLowerCase()
-var vowelCount = 0
-for(var i = 0; i<arg.length; i++) {
-  if (arg[i] === "a" || arg[i] === "e" || arg[i] === "i" || arg[i] === "o" || arg[i] === "u") {
-    vowelCount ++;
-  }
-}
-console.log(vowelCount);
-
-//
-// BONUS 1
 // var arg = process.argv[2].toLowerCase()
-// var vowelcheck = 0
-// var vowelspot = []
 // var vowelCount = 0
 // for(var i = 0; i<arg.length; i++) {
 //   if (arg[i] === "a" || arg[i] === "e" || arg[i] === "i" || arg[i] === "o" || arg[i] === "u") {
-//     vowelCount ++
-//     vowelspot.push(i)
-//     if (i === arg.length - 1 && vowelCount > 1){
-//       for(var j = 0; j<vowelspot.length; j++) {
-//         if (vowelspot[j] - vowelspot[j + 1] === -1 ) {
-//           console.log("nope")
-//           break
-//         } else if (j === vowelspot.length - 1){
-//           console.log("it checks out")
-//         }
-//       }
-//     }
+//     vowelCount ++;
 //   }
 // }
-//
-// Bonus 2
+// console.log(vowelCount);
+// 
+// BONUS 1
+var arg = process.argv[2].toLowerCase()
+var vowelcheck = 0
+var vowelspot = []
+var vowelCount = 0
+for (var i = 0; i < arg.length; i++) {
+    if (arg[i] === "a" || arg[i] === "e" || arg[i] === "i" || arg[i] === "o" || arg[i] === "u") {
+        vowelCount++
+        vowelspot.push(i)
+    }
+    if (i === arg.length - 1 && vowelCount > 1) {
+        for (var j = 0; j < vowelspot.length; j++) {
+            console.log("24")
+            if (vowelspot[j] - vowelspot[j + 1] === -1) {
+                console.log("nope")
+                break
+            } else if (j === vowelspot.length - 1) {
+                console.log("it checks out")
+            }
+        }
+    }
+}
+
+// 
+// // Bonus 2
 // var arg = process.argv[2].toLowerCase()
 // var consonant = []
 // var vowelcheck = 0
@@ -56,42 +56,40 @@ console.log(vowelCount);
 //     }
 //   }
 // }
-//
-//
-// stuck in the middle with args
+// 
+// // stuck in the middle with args
 // var arg1 = parseInt(process.argv[2])
 // var arg2 = parseInt(process.argv[3])
 // var lower
 // var upper
 // if (arg1 > arg2) {
-//   upper = arg1;
-//   lower = arg2;
+//     upper = arg1;
+//     lower = arg2;
 // } else {
-//   upper = arg2;
-//   lower = arg1;
+//     upper = arg2;
+//     lower = arg1;
 // }
-//
-// var rando = Math.floor(Math.random()*10)
+// var rando = Math.floor(Math.random() * 10)
 // if (lower > rando) {
-//   console.log("too cold")
-//   console.log(rando + " " + lower + " " + upper)
+//     console.log("too cold")
+//     console.log(rando + " " + lower + " " + upper)
 // } else if (upper < rando) {
-//   console.log("too hot")
-//   console.log(lower + " " + upper + " " + rando)
+//     console.log("too hot")
+//     console.log(lower + " " + upper + " " + rando)
 // } else {
-//   console.log("just right")
-//   console.log(lower + " " + rando + " " + upper)
+//     console.log("just right")
+//     console.log(lower + " " + rando + " " + upper)
 // }
-//
-// even on a jet plane
+// 
+// // even on a jet plane
 // var stringArr = process.argv
 // for (var i = 2; i<stringArr.length; i++) {
 //   if (stringArr[i].length % 2 === 0) {
 //     console.log(stringArr[i])
 //   }
 // }
-//
-// we want the func
+// 
+// // we want the func
 // var num = process.argv[2]
 // var string = process.argv[3]
 // var evenator = function(num, string) {
@@ -101,10 +99,10 @@ console.log(vowelCount);
 //     console.log(string.split("").reverse().join(""))
 //   }
 // }
+// 
 // evenator(num, string)
-//
-//
-// gotta have the func
+// 
+// // gotta have the func
 // var counter = 0
 // var sayHi = function() {
 //   console.log("BASS IN YER FACE");
@@ -113,11 +111,9 @@ console.log(vowelCount);
 //     clearInterval(refreshIntervalId);
 //   };
 // }
-//
 // var refreshIntervalId = setInterval(sayHi, 3000);
-//
-// foreman swears he learned his math
-//
+// 
+// // foreman swears he learned his math
 // var Dozer = function(name, color) {
 //   this.name = name;
 //   this.color = color;
@@ -129,20 +125,19 @@ console.log(vowelCount);
 // console.log(Bob.name)
 // console.log(Bob.color)
 // Bob.backup()
-//
-// it's a final countdown
+// 
+// // it's a final countdown
 // var body = document.querySelector("body")
 // var p = document.createElement("p")
 // body.appendChild(p)
 // var counter = 10
 // var countdown = function() {
-//   p.innerText = counter;
-//   counter --;
-//   if(counter < 0){
-//     p.innerText = 0;
-//     body.setAttribute("style", "background-color: red; color: white")
-//     clearInterval(refreshIntervalId);
-//   }
+//     p.innerText = counter;
+//     counter--;
+//     if (counter < 0) {
+//         p.innerText = 0;
+//         body.setAttribute("style", "background-color: red; color: white")
+//         clearInterval(refreshIntervalId);
+//     }
 // }
-//
 // var refreshIntervalId = setInterval(countdown, 1000);
