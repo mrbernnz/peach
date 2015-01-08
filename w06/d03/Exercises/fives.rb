726927e8ee62570793e1944b305001cc12c0ec0d
Bernz <BernzAir@BernzAir.home>
2015-01-07 19:49:59 -0500

    Test in sublime

--- w06/d03/Exercises/fives.rb
+++ w06/d03/Exercises/fives.rb
@@ -13,22 +13,22 @@
 
 # sam's solution
 def multi_five?(number)
-	return number % 5 == 0
+  return number % 5 == 0
 end
 
 get_fives = true
 while get_fives
-	puts "Enter a number or quit."
-	number = gets.chomp
-	if number == "quit"
-		puts "Goodbye"
-		get_fives = false
-	else
-		multiples_of_five = multi_five?(number.to_i)
-		if multiples_of_five
-			puts number + " is a multiple of 5"
-		else
-			puts number + " is not a multiple of 5"
-		end
-	end
+  puts "Enter a number or quit."
+  number = gets.chomp
+  if number == "quit"
+    puts "Goodbye"
+    get_fives = false
+  else
+    multiples_of_five = multi_five?(number.to_i)
+    if multiples_of_five
+      puts number + " is a multiple of 5"
+    else
+      puts number + " is not a multiple of 5"
+    end
+  end
 end
