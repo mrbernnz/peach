43f08e99407394ec452403b5986f1cfc05fa13fb
Bernz <BernzAir@BernzAir.local>
2015-01-20 17:26:12 -0500

    lesson

---
+++ w08/d01/expr/server.js
@@ -0,0 +1,37 @@
+// var express = require('express');
+// var request = require('request');
+// var url = 'http://api.randomuser.me/';
+// request(url,function(error,response,body) {
+//   if(!error && response.statusCode == 200) {
+//     var data = JSON.parse(body);
+//     console.log(data.results[0]);
+//     console.log(data.results[0].user.name.first); /*syntax specific*/
+//   };
+// });
+var express = require('express');
+var app = express();
+var request = require('request');/*be careful to not overide 'parameter' in app.get*/
+app.get('/', function(req, res) /*you will never use req, unless you are grabbing information from the header*/
+{
+  var url = 'http://api.randomuser.me/';
+    // var person_json; 
+    request(url, function(error, response, body) 
+    {
+      if (!error && response.statusCode === 200) 
+      {
+        var data = JSON.parse(body);
+        var person = 
+        {
+          first: data['results'][0]['user']['name']['first'],
+          last: data['results'][0]['user']['name']['last'],
+          email: data['results'][0]['user']['email'],
+          username: data['results'][0]['user']['username']
+        };
+        res.json(person);
+      };
+  });
+});
+
+var server = app.listen(3000, function() {
+  console.log('server running on 3000')
+});
--- w08/d01/server.rb
+++ w08/d01/server.rb
@@ -4,8 +4,8 @@ require 'pry'
 # num = 8
 
 require 'sinatra'
-require 'JSON'
-# get '/' do 
+# require 'JSON'
+# get '/' do
 #   content_type :json
 #   data = {messsage: "Hello World"}
 #   data.to_json
@@ -17,12 +17,32 @@ require 'JSON'
 #   data.to_json
 # end
 
+# get '/' do
+#   content_type :json
+#   data = { "results" => [
+#     {"Jurassic Park" => 9},
+#     {"Superman" => 2},
+#     {"Inception" => 8}
+#   ]}
+#   data.to_json
+# end
+
+require 'httparty'
+# url = 'http://api.randomuser.me'
+# response = HTTParty.get(url)
+# binding.pry
+
+
 get '/' do
   content_type :json
-  data = { "results" => [
-    {"Jurassic Park" => 9},
-    {"Superman" => 2},
-    {"Inception" => 8}
-  ]}
-  data.to_json
+  url = 'http://api.randomuser.me/'
+  response = HTTParty.get(url)
+  # person = response['results'][0]['user']
+  person = {
+    first: response['results'][0]['user']['name']['first'],
+    last: response['results'][0]['user']['name']['last'],
+    email: response['results'][0]['user']['email'],
+    username: response['results'][0]['user']['username']
+  }
+  person.to_json
 end
