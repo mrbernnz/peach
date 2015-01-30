d081dba4ee52948b4e809c732ce7637d76382b55
Bernz <BernzAir@BernzAir.local>
2015-01-30 14:43:51 -0500

    folder delete

--- w09/d05 Ruby Classes & Inheritance, SQL Joins/got_ar/lib/connection.rb
+++
@@ -1,6 +0,0 @@
-require 'active_record'
-ActiveRecord::Base.establish_connection({
-  :adapter => 'sqlite3',
-  :database => 'got.db'
-  })
-ActiveRecord::Base.logger = Logger.new(STDOUT);
--- w09/d05 Ruby Classes & Inheritance, SQL Joins/got_ar/lib/house.rb
+++
@@ -1,7 +0,0 @@
-require 'active_record'
-class House < ActiveRecord::Base
-  def characters
-    Character.where({house_id: self.id})
-  end
-end
-# slytherin.characters will to thru the characters table and return all characters that have the same house_id as slytherin's id.
